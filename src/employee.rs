2ba61a111a725bc015566685b8b4ae77b4940f1b
dizzyplay <vovoboss@gmail.com>
2022-12-11 20:39:54 +0900

    iterator

--- src/employee.rs
+++ src/employee.rs
@@ -1,18 +1,53 @@
 use crate::printinfo::PrintInfo;
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub struct Employee {
+    pub id: usize,
     pub name: String,
 }
 
 impl Employee {
-    pub fn new(name: String) -> Self {
-        Employee { name }
+    pub fn new(id: usize, name: String) -> Self {
+        Employee { name, id }
     }
 }
 
 impl PrintInfo for Employee {
     fn print_info(&self) {
         println!(" name is {}", self.name);
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct EmployeeRecords {
+    idx: usize,
+    employees: Vec<Employee>,
+}
+
+impl EmployeeRecords {
+    pub fn new() -> Self {
+        Self {
+            idx: 0,
+            employees: Vec::new(),
+        }
+    }
+    pub fn push(&mut self, employee: Employee) {
+        self.employees.push(employee);
+    }
+    pub fn get(&self, id: usize) -> Option<&Employee> {
+        self.employees.iter().find(|e| e.id == id)
+    }
+}
+
+impl Iterator for EmployeeRecords {
+    type Item = Employee;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.employees.len() > self.idx {
+            let output = self.employees[self.idx].clone();
+            self.idx += 1;
+            return Some(output);
+        } else {
+            return None;
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,31 @@ mod employee;
 mod printinfo;
 mod product;
 
-use employee::Employee;
+use employee::{Employee, EmployeeRecords};
 use printinfo::{dynamic_dispatch, static_dispatch};
 use product::Product;
 
 fn main() {
-    let e = Employee::new("hello".to_string());
-    let p = Product::new(1, "p".to_string());
-    static_dispatch(e.clone());
-    dynamic_dispatch(Box::new(e));
+    let john = Employee::new(101, "John".to_string());
+    let jane = Employee::new(102, "Jane".to_string());
+    let tom = Employee::new(103, "Tom".to_string());
 
-    static_dispatch(p.clone());
-    dynamic_dispatch(Box::new(p));
+    let mut employee_records = EmployeeRecords::new();
+    employee_records.push(john);
+    EmployeeRecords::push(&mut employee_records, jane);
+    employee_records.push(tom);
+
+    println!("{:#?}", employee_records);
+    println!(":Get 101: {:?}", employee_records.get(101));
+    println!(":Get 102: {:?}", employee_records.get(102));
+
+    println!("{:#?}", employee_records);
+    println!(" next: {:?}", employee_records.next());
+    println!("{:#?}", employee_records);
+    println!(":Get 100: {:?}", employee_records.get(100));
+
+    for em in &mut employee_records {
+        println!("For {:#?}", em);
+    }
+    println!("{:#?}", employee_records);
 }
