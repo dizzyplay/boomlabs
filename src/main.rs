bd9f0ab16a821c0e77d5bc688fe8dd4b052d9692
dizzyplay <vovoboss@gmail.com>
2022-12-25 22:29:24 +0900

    format example double free error

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,14 @@
+use std::mem;
+
 mod employee;
 mod printinfo;
 mod product;
 
-use employee::{Employee, EmployeeRecords};
-use printinfo::{dynamic_dispatch, static_dispatch};
-use product::Product;
-
 fn main() {
-    let john = Employee::new(101, "John".to_string());
-    let jane = Employee::new(102, "Jane".to_string());
-    let tom = Employee::new(103, "Tom".to_string());
-
-    let mut employee_records = EmployeeRecords::new();
-    employee_records.push(john);
-    EmployeeRecords::push(&mut employee_records, jane);
-    employee_records.push(tom);
-
-    println!("{:#?}", employee_records);
-    println!(":Get 101: {:?}", employee_records.get(101));
-    println!(":Get 102: {:?}", employee_records.get(102));
-
-    println!("{:#?}", employee_records);
-    println!(" next: {:?}", employee_records.next());
-    println!("{:#?}", employee_records);
-    println!(":Get 100: {:?}", employee_records.get(100));
-
-    for em in &mut employee_records {
-        println!("For {:#?}", em);
-    }
-    println!("{:#?}", employee_records);
+    let mut v = vec![65, 122];
+    let s = unsafe { String::from_raw_parts(v.as_mut_ptr(), v.len(), v.capacity()) };
+    println!("{:?}", v);
+    mem::forget(v);
+    assert_eq!(s, "Az");
+    mem::drop(s);
 }
